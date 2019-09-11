60f280137538a88c5c408e69510dfbfdd84176a1
Abul Hashem Jr <abul@limosys.com>
2019-09-11 14:21:20 -0400

    Adds splash screen

--- app/src/main/java/com/project/neardoc/view/fragments/Login.kt
+++ app/src/main/java/com/project/neardoc/view/fragments/Login.kt
@@ -6,28 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 
 import com.project.neardoc.R
 import com.project.neardoc.di.Injectable
 import dagger.android.support.AndroidSupportInjection
-import kotlinx.android.synthetic.main.fragment_registration.*
 
-class Registration : Fragment(), Injectable{
+class Login : Fragment(), Injectable {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         AndroidSupportInjection.inject(this)
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        registration_page_create_account_bt_id.setOnClickListener{
-            val navToWelcome = RegistrationDirections.actionWelcomeFragment()
-            Navigation.findNavController(it).navigate(navToWelcome)
-        }
     }
 }
