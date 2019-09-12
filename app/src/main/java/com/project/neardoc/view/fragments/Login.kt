03bfe64967012371c43b5b6486ab5ae96730b69d
Abul Hashem Jr <abul@limosys.com>
2019-09-12 14:23:20 -0400

    Designs login page

--- app/src/main/java/com/project/neardoc/view/fragments/Login.kt
+++ app/src/main/java/com/project/neardoc/view/fragments/Login.kt
@@ -2,16 +2,26 @@ package com.project.neardoc.view.fragments
 
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 
 import com.project.neardoc.R
 import com.project.neardoc.di.Injectable
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class Login : Fragment(), Injectable {
+class Login : Fragment(), Injectable, CoroutineScope {
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = this.job + Dispatchers.Main
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +33,8 @@ class Login : Fragment(), Injectable {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val animation: Animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_in)
+        fragment_login_logo_text_view.startAnimation(animation)
+        fragment_login_container_id.startAnimation(animation)
     }
 }
