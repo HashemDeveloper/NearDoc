b5f6440087bfa199b36037d5bf38e3bb8ae1e3bf
Abul Hashem Jr <abul@limosys.com>
2019-10-01 00:16:23 -0400

    Feature sign in with google

--- app/src/main/java/com/project/neardoc/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/project/neardoc/viewmodel/LoginViewModel.kt
@@ -1,14 +1,14 @@
 package com.project.neardoc.viewmodel
 
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.project.neardoc.rxauth.IRxAuthentication
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(): ViewModel() {
-    @Inject
-    lateinit var googleSignInClient: GoogleSignInClient
     @Inject
     lateinit var iRxAuthentication: IRxAuthentication
 
@@ -17,5 +17,9 @@ class LoginViewModel @Inject constructor(): ViewModel() {
     override fun onCleared() {
         super.onCleared()
         this.compositeDisposable.clear()
+    }
+
+    fun processLoginWithGoogle(activity: FragmentActivity?, accountIfo: GoogleSignInAccount) {
+
     }
 }
