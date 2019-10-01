2a0a7d49d27eb442788ab61b7dc712907114d732
Abul Hashem Jr <abul@limosys.com>
2019-10-01 15:44:22 -0400

    Fixes bug in view model

--- app/src/main/java/com/project/neardoc/rxauth/RxAuthentication.kt
+++ app/src/main/java/com/project/neardoc/rxauth/RxAuthentication.kt
@@ -9,29 +9,18 @@ import io.reactivex.Observable
 import javax.inject.Inject
 
 class RxAuthentication @Inject constructor(): IRxAuthentication {
-    private val isLoading: MutableLiveData<Boolean> = MutableLiveData()
-    private val isError: MutableLiveData<Boolean> = MutableLiveData()
-    private val isLoginSuccess: MutableLiveData<Boolean> = MutableLiveData()
-
     override fun googleSignIn(
         activity: FragmentActivity,
         firebaseAuth: FirebaseAuth,
         authCredential: AuthCredential
     ): Observable<FirebaseUser> {
-        this.isLoading.value = true
         return Observable.create { emitter ->
             firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(activity) {
                 if (it.isSuccessful) {
                     val firebaseUser = firebaseAuth.currentUser
-                    this.isError.value = false
-                    this.isLoading.value = false
-                    this.isLoginSuccess.value = true
                     emitter.onNext(firebaseUser!!)
                 }
             }.addOnFailureListener{
-                this.isError.value = true
-                this.isLoading.value = false
-                this.isLoginSuccess.value = false
                 emitter.onError(it)
             }
         }
