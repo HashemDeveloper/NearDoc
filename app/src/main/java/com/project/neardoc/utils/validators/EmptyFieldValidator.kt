ff630cdc6b4321db4dfe2369472a73967fd9d7ba
Abul Hashem Jr <abul@limosys.com>
2019-10-03 11:14:13 -0400

    Performs input validations

--- app/src/main/java/com/project/neardoc/utils/validators/EmptyFieldValidator.kt
+++ app/src/main/java/com/project/neardoc/utils/validators/EmptyFieldValidator.kt
@@ -3,9 +3,9 @@ package com.project.neardoc.utils.validators
 import com.google.android.material.textfield.TextInputLayout
 import com.project.neardoc.R
 
-class NameValidator(mContainer: TextInputLayout): BaseInputValidator(mContainer) {
+class EmptyFieldValidator(mContainer: TextInputLayout): BaseInputValidator(mContainer) {
     init {
-        mErrorMessage = mContainer.resources.getString(R.string.name_required)
+        mEmptyMessage = mContainer.resources.getString(R.string.name_required)
     }
 
     override fun isValid(charSequence: CharSequence): Boolean {
--- app/src/main/java/com/project/neardoc/view/fragments/Registration.kt
+++ app/src/main/java/com/project/neardoc/view/fragments/Registration.kt
@@ -13,6 +13,10 @@ import com.project.neardoc.R
 import com.project.neardoc.di.Injectable
 import com.project.neardoc.events.NetworkStateEvent
 import com.project.neardoc.utils.ConnectionSettings
+import com.project.neardoc.utils.validators.EmailValidator
+import com.project.neardoc.utils.validators.EmptyFieldValidator
+import com.project.neardoc.utils.validators.PasswordValidator
+import com.project.neardoc.utils.validators.UsernameValidator
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_registration.*
 import org.greenrobot.eventbus.EventBus
@@ -23,6 +27,10 @@ class Registration : Fragment(), Injectable{
     private var isInternetAvailable: Boolean = false
     private val mobileData = "MOBILE_DATA"
     private val wifiData = "WIFI_DATA"
+    private var emptyFieldValidator: EmptyFieldValidator? = null
+    private var usernameValidator: UsernameValidator? = null
+    private var emailValidator: EmailValidator? = null
+    private var passwordValidator: PasswordValidator?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,13 +42,20 @@ class Registration : Fragment(), Injectable{
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         performNavigationActions()
+        registerInputValidators()
+    }
+    private fun registerInputValidators() {
+        this.emptyFieldValidator = EmptyFieldValidator(fragment_register_full_name_input_layout_id)
+        this.usernameValidator = UsernameValidator(fragment_register_username_input_layout_id)
+        this.emailValidator = EmailValidator(fragment_register_email_input_layout_id)
+        this.passwordValidator = PasswordValidator(fragment_register_password_input_layout_id)
+
     }
     private fun performNavigationActions() {
         fragment_registration_create_account_bt_id.setOnClickListener {
             if (this.isInternetAvailable) {
-                // perform registration process
+               processRegistration()
             } else {
                 displayConnectionSetting()
             }
@@ -50,6 +65,22 @@ class Registration : Fragment(), Injectable{
             Navigation.findNavController(it).navigate(navigateToLoginPage)
         }
     }
+    private fun processRegistration() {
+        val fullName: String = fragment_register_full_name_edit_text_id.text.toString()
+        val username: String = fragment_register_username_edit_text_id.text.toString()
+        val email: String = fragment_register_email_edit_text_id.text.toString()
+        val password: String = fragment_register_password_edit_text_id.text.toString()
+
+        val isNameValidated: Boolean = this.emptyFieldValidator!!.getIsValidated(fullName)
+        val isUsernameValidated: Boolean = this.usernameValidator!!.getIsValidated(username)
+        val isEmailValidated: Boolean = this.emailValidator!!.getIsValidated(email)
+        val isPasswordValidated: Boolean = this.passwordValidator!!.getIsValidated(password)
+
+        if (isNameValidated && isUsernameValidated && isEmailValidated && isPasswordValidated) {
+            Toast.makeText(activity, "Register", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     private fun displayConnectionSetting() {
         val connectionSettings = ConnectionSettings(activity!!, view!!)
         connectionSettings.initWifiSetting(false)
