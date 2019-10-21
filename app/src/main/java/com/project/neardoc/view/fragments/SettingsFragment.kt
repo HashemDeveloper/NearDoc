7144df0f6e2fb63331cc5131bead7383b910e6ef
Abul Hashem Jr <abul@limosys.com>
2019-10-21 12:51:36 -0400

    Sets pref listeners

--- app/src/main/java/com/project/neardoc/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/project/neardoc/view/fragments/SettingsFragment.kt
@@ -1,13 +1,16 @@
 package com.project.neardoc.view.fragments
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.project.neardoc.R
 import com.project.neardoc.di.Injectable
+import com.project.neardoc.view.widgets.CustomPreference
 import dagger.android.support.AndroidSupportInjection
 
 class SettingsFragment: PreferenceFragmentCompat(), Injectable {
-
+    private var list: List<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidSupportInjection.inject(this)
@@ -15,5 +18,43 @@ class SettingsFragment: PreferenceFragmentCompat(), Injectable {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_pref_layout, rootKey)
+        addPrefKeys()
+        setupPreferenceListeners()
+    }
+    private fun addPrefKeys() {
+        this.list = listOf("prefSignInSecKey", "prefDistanceKey", "prefSetLimitKey",
+            "prefContactUsKey", "prefRateKey", "prefSendFeedbackKey", "prefTermsAndConditionKey", "prefPrivacyPolicyKey")
+    }
+    private fun getPrefKeys(): List<String>? {
+        if (this.list != null) {
+            return this.list!!
+        } else {
+            return null
+        }
+    }
+    private fun setupPreferenceListeners() {
+        for (keys in this.getPrefKeys()!!) {
+            val prefSignAndSec: CustomPreference = findPreference<CustomPreference>(keys) as CustomPreference
+            prefSignAndSec.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                if (it.key == "prefSignInSecKey") {
+                    Toast.makeText(context, "SignInKey", Toast.LENGTH_SHORT).show()
+                } else if (it.key == "prefDistanceKey") {
+                    Toast.makeText(context, "SetDistance", Toast.LENGTH_SHORT).show()
+                } else if (it.key == "prefSetLimitKey") {
+                    Toast.makeText(context, "SetLimit", Toast.LENGTH_SHORT).show()
+                } else if (it.key == "prefContactUsKey") {
+                    Toast.makeText(context, "ContactUs", Toast.LENGTH_SHORT).show()
+                } else if (it.key == "prefRateKey") {
+                    Toast.makeText(context, "RateUs", Toast.LENGTH_SHORT).show()
+                } else if (it.key == "prefSendFeedbackKey") {
+                    Toast.makeText(context, "SendFeedback", Toast.LENGTH_SHORT).show()
+                } else if (it.key == "prefTermsAndConditionKey") {
+                    Toast.makeText(context, "TermsAndCond", Toast.LENGTH_SHORT).show()
+                } else if (it.key == "prefPrivacyPolicyKey") {
+                    Toast.makeText(context, "PrivacyPol", Toast.LENGTH_SHORT).show()
+                }
+                true
+            }
+        }
     }
 }
