5b75d4b5203b45f6e762a11a73b7bfa5ba0ea0a4
Abul Hashem Jr <abul@limosys.com>
2019-11-04 23:22:56 -0500

    Delete account confirmation dialog

--- app/src/main/java/com/project/neardoc/view/settings/SignInSecurity.kt
+++ app/src/main/java/com/project/neardoc/view/settings/SignInSecurity.kt
@@ -1,16 +1,24 @@
 package com.project.neardoc.view.settings
 
 
-import android.content.DialogInterface
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
+import androidx.core.view.setPadding
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 import com.project.neardoc.R
 import com.project.neardoc.data.local.ISharedPrefService
@@ -22,6 +30,7 @@ import com.project.neardoc.model.SignInSecurityHeaderModel
 import com.project.neardoc.model.SignInSecurityModel
 import com.project.neardoc.utils.Constants
 import com.project.neardoc.utils.PageType
+import com.project.neardoc.utils.validators.PasswordValidator
 import com.project.neardoc.view.adapters.SignInSecClickListener
 import com.project.neardoc.view.adapters.SignInSecurityAdapter
 import dagger.android.support.AndroidSupportInjection
@@ -30,9 +39,11 @@ import org.greenrobot.eventbus.EventBus
 import javax.inject.Inject
 
 class SignInSecurity : Fragment(), Injectable, SignInSecClickListener {
-    private var signInSecurityAdapter: SignInSecurityAdapter?= null
+    private var passwordValidator: PasswordValidator? = null
+    private var signInSecurityAdapter: SignInSecurityAdapter? = null
     @Inject
     lateinit var iSharedPrefService: ISharedPrefService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidSupportInjection.inject(this)
         super.onCreate(savedInstanceState)
@@ -76,19 +87,59 @@ class SignInSecurity : Fragment(), Injectable, SignInSecClickListener {
                     bundle.putString(Constants.WORKER_EMAIL, items.email)
                     updateEmailAction.navigate(R.id.updateEmail, bundle)
                 } else {
-                   val updatePasswordAction = findNavController()
+                    val updatePasswordAction = findNavController()
                     updatePasswordAction.navigate(R.id.updatePassword)
                 }
             }
             is ManageAccountModel -> {
-                val alertDialog = MaterialAlertDialogBuilder(this.context)
-                alertDialog.setTitle("Delete Account")
-                alertDialog.setMessage("Are you sure? All your information will be deleted!")
-                alertDialog.setPositiveButton("OK") { onClick, i ->
-                    Toast.makeText(this.context, "Deleted", Toast.LENGTH_SHORT).show()
-                }
-                alertDialog.show()
+                deleteAccountDialog()
             }
         }
+    }
+
+    private fun deleteAccountDialog() {
+        val viewGroup: ViewGroup = activity!!.window.decorView.rootView as ViewGroup
+        val dView: View =
+            layoutInflater.inflate(R.layout.layout_delete_account_dialog, viewGroup, false)
+        val passwordInputLayout: TextInputLayout =
+            dView.findViewById(R.id.layout_delete_account_pass_input_layout_id)
+        this.passwordValidator = PasswordValidator(passwordInputLayout)
+        val passwordTextView: TextInputEditText =
+            dView.run { findViewById(R.id.layout_delete_account_pass_edit_text_id) }
+        val cancelBt: MaterialButton =
+            dView.run { findViewById(R.id.layout_delete_account_cancel_bt_id) }
+        val deleteBt: MaterialButton =
+            dView.run { findViewById(R.id.layout_delete_account_delete_bt_id) }
+
+        val accountInfoDialog = MaterialAlertDialogBuilder(this.context)
+        val confirmDeleteDialog = MaterialAlertDialogBuilder(this.context)
+        confirmDeleteDialog.setTitle(getString(R.string.delete_account))
+        confirmDeleteDialog.setMessage(getString(R.string.confirm_delete_account))
+        confirmDeleteDialog.setNegativeButton(android.R.string.cancel) { _, _ ->
+
+        }
+        confirmDeleteDialog.setPositiveButton(android.R.string.ok) { _, _ ->
+            accountInfoDialog.setTitle("Please enter your password")
+            accountInfoDialog.setMessage("juniayulia@outlook.com")
+            accountInfoDialog.setView(dView)
+            val alertDialog = accountInfoDialog.create()
+            alertDialog.show()
+            cancelBt.setOnClickListener {
+                alertDialog.dismiss()
+            }
+            deleteBt.setOnClickListener {
+                val password: String = passwordTextView.text.toString()
+                val isValidPass: Boolean = this.passwordValidator?.getIsValidated(password)!!
+                if (isValidPass) {
+                    processDeleteAccount(password)
+                    alertDialog.dismiss()
+                }
+            }
+        }
+        confirmDeleteDialog.show()
+    }
+
+    private fun processDeleteAccount(password: String) {
+        Log.i("Password: ", password)
     }
 }
