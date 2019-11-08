cda66a30fa6ded8024e78a6ae06482984eca7886
Abul Hashem Jr <abul@limosys.com>
2019-11-08 16:15:59 -0500

    Send email from app

--- app/src/main/java/com/project/neardoc/view/settings/ContactUs.kt
+++ app/src/main/java/com/project/neardoc/view/settings/ContactUs.kt
@@ -1,14 +1,15 @@
 package com.project.neardoc.view.settings
 
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-
 import com.project.neardoc.R
 import com.project.neardoc.di.Injectable
 import com.project.neardoc.events.LandInSettingPageEvent
@@ -48,7 +49,25 @@ class ContactUs : Fragment(), Injectable, ContactUsAdapter.ContactClickListener 
     }
 
     override fun onEmailClicked(email: String) {
-        Toast.makeText(context!!, "Email: $email", Toast.LENGTH_SHORT).show()
+        sendEmail(email)
+    }
+
+    private fun sendEmail(email: String) {
+        val i = Intent(Intent.ACTION_SEND)
+        i.type = "message/rfc822"
+        i.putExtra(
+            Intent.EXTRA_EMAIL,
+            arrayOf(email)
+        )
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."))
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(
+                context!!,
+                "There are no email clients installed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onDestroy() {
