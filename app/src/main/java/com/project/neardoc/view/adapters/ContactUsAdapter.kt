7ff9116ccd9d2cf9582b96966279063093b3f6c2
Abul Hashem Jr <abul@limosys.com>
2019-11-08 14:07:34 -0500

    Makes variable of constant class const val

--- app/src/main/java/com/project/neardoc/view/adapters/ContactUsAdapter.kt
+++ app/src/main/java/com/project/neardoc/view/adapters/ContactUsAdapter.kt
@@ -6,12 +6,17 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ContactUsAdapter constructor(private val context: Context): RecyclerView.Adapter<BaseViewHolder<*>>() {
 
+    companion object {
+        private const val TYPE_HEADER = 0
+    }
+    private var data: List<Any> = emptyList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       return this.data.size
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
